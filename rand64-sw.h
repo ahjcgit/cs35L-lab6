3d81451706c6772ac5fe3d19f9628e4273a078ac
ahjcgit <ahjcuniv@ucla.edu>
2024-03-13 02:41:21 -0700

    f

--- rand64-sw.h
+++ rand64-sw.h
@@ -1,7 +1,7 @@
 #ifndef RAND64_SW_H
 #define RAND64_SW_H
 
-void software_rand64_init (const char* file_dir, int use_lrand);
+void software_rand64_init (void);
 unsigned long long software_rand64 (void);
 void software_rand64_fini (void);
 _Bool writebytes(long long x, int nbytes);
